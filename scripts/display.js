3fd7b3d3c0dce5fdfad042efde591294e79fd87b
Nicholas Harper <harpergnicholas@gmail.com>
2025-06-02 21:33:26 +0900

    Display functions in their own script

---
+++ scripts/display.js
@@ -0,0 +1,65 @@
+import { gameState, upgrades, gameStages } from "./data.js";
+import { createDot } from "./consumables.js";
+import { formatNum } from "./util.js";
+
+const scoreDisplay = document.getElementById('score');
+
+
+export function initDisplay(){
+    //Set display to only have upgrades at first
+    $("#left-container").children().hide();
+    $("#upgrades-container").show();
+    $("#right-container").children().hide();
+    $("#stats-container").show();
+    $("#nomscendUpgradesBttn").hide(); 
+    $("#nomsecReqText").text(`Lifetime Score Required to Nomscend: ${formatNum(gameState.nomsecScoreReq)}`);
+    $("#nomscensionBttn").hide();
+    $("#nomCoinDisplay").hide();
+    $("#upgradeAutoFeedSpeed").parent().hide();
+
+
+    if (gameState.nomscentionUnlocked) {
+        $("#nomscendUpgradesBttn").show(); 
+        $("#nomscensionBttn").show();
+        $("#nomCoinDisplay").show();
+    }
+    if (upgrades.autoFeed.enabled){
+        $("#upgradeAutoFeedSpeed").parent().show();
+        gameState.dotIntervalID = setInterval(createDot, upgrades.autoFeed.speed*1000);
+    }
+}
+
+export function updateStats(){
+    scoreDisplay.textContent = formatNum(gameState.score);
+    $("#lifetimeScoreText").text(formatNum(gameState.liftimeScore));
+    $("#dotBaseValueText").text(formatNum(gameState.dotValue));
+    $("#dotSpeedText").text(gameState.dotSpeed);
+    $("#dotsEatenText").text(formatNum(gameState.dotsEaten));
+    $("#dotMultiText").text(formatNum(gameState.dotMulti));
+    $("#autofeedSpeedText").text(upgrades.autoFeed.speed);
+    $("#nomscensionCountText").text(formatNum(gameState.nomscensionCount));
+    $("#nomCoinsText").text(formatNum(gameState.nomCoins));
+    $("#nomCoinStatText").text(formatNum(gameState.nomCoins));
+
+    //Update Nomsension coins
+    $("#nomCoinGainsText").text(formatNum(gameState.nomscendScore.divide(100000)));
+}
+
+
+    //Upgrade progressBar based on score
+export function updateProgressBar() {
+    if (gameStages[gameState.stage].statType == "score"){
+        var stat = gameState.liftimeScore;
+    } else if (gameStages[gameState.stage].statType == "nomscend"){
+        var stat = gameState.nomscensionCount;
+    } else if (gameStages[gameState.stage].statType == "cap"){
+        var stat = gameState.liftimeScore;
+    }
+    let progressBar = document.querySelector('.progress-bar');
+    let progressBarText = document.getElementById('progress-text');
+    let maxScore = gameStages[gameState.stage].requirement;
+    let progress = (stat.divideBy(maxScore).toNumber())*100;
+
+    progressBar.style.width = progress + "%";
+    progressBarText.innerHTML = `${gameStages[gameState.stage].progressText} - (${progress.toFixed(2)}%)`;
+}
