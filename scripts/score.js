6773f52f64b05852663a0c4ebc92af718d5a4d33
Nicholas Harper <harpergnicholas@gmail.com>
2025-06-02 21:37:01 +0900

    Changed but put back

--- scripts/score.js
+++ scripts/score.js
@@ -1,7 +1,7 @@
 import { gameState } from './data.js';
 
 export function increaseScore(){
-    const value =  gameState.dotValue.times(gameState.dotMulti.plus(gameState.nomscendMultiValue));
+    const value =  gameState.dotValue.times(gameState.dotMulti);
     gameState.score = gameState.score.plus(value);
     gameState.liftimeScore = gameState.liftimeScore.plus(value);
     gameState.dotsEaten = gameState.dotsEaten.plus(1);
