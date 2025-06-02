0003348b1497070b903c16f972f44446dc3c85a0
Nicholas Harper <harpergnicholas@gmail.com>
2025-06-02 21:35:33 +0900

    Added upgrade Buttons effects. Increase Speed still needs to be fixed

--- scripts/upgradeButtons.js
+++ scripts/upgradeButtons.js
@@ -2,6 +2,10 @@ import { gameState } from './data.js';
 import { upgrades } from './data.js';
 import { buttonBought } from './buttonHandling.js';
 import { createDot } from './consumables.js';
+import { increaseCost } from './util.js';
+
+
+
 
 export function upgradeDotValue(){
     let incDotVal = upgrades.increaseDotValue;
@@ -13,15 +17,13 @@ export function upgradeDotValue(){
         //Update dot cost
         incDotVal.cost = incDotVal.cost.times(incDotVal.upgradeScale);
         upgrades.increaseDotValue.level = upgrades.increaseDotValue.level +1;
+        $("#upgradeDotValueLvl").text(`Level: ${incDotVal.level}`);
     }
 }
 
 
 export function upgradeDotSpeed(){
     let incDotSpeed = upgrades.increaseDotSpeed;
-    if (incDotSpeed.level >= incDotSpeed.maxlevel){
-        buttonBought("upgradeDotSpeed");
-    }
     if (gameState.score.greaterThanOrEqualTo(incDotSpeed.cost)){
         //Reduce score
         gameState.score = gameState.score.minus(incDotSpeed.cost);
@@ -30,11 +32,28 @@ export function upgradeDotSpeed(){
         //Update dot cost
         incDotSpeed.cost = incDotSpeed.cost.times(incDotSpeed.upgradeScale);
         upgrades.increaseDotSpeed.level = upgrades.increaseDotSpeed.level + 1;
+        $("#upgradeDotSpeedLvl").text(`Level: ${incDotSpeed.level}`);
     }
 }
 
+export function upgradeDotMulti(){    
+    let incDotMulti = upgrades.increaseDotMulti;
+    if (gameState.score.greaterThanOrEqualTo(incDotMulti.cost)){
+        //Reduce score
+        gameState.score = gameState.score.minus(incDotMulti.cost);
+        //Update dot Value
+        gameState.dotMulti = gameState.dotMulti.times(incDotMulti.increase);
+        //Update dot cost
+        incDotMulti.cost = incDotMulti.cost.times(incDotMulti.upgradeScale);
+        //Increase level
+        upgrades.increaseDotMulti.level = upgrades.increaseDotMulti.level+1;
+        $("#upgradeDotMultiLvl").text(`Level: ${incDotMulti.level}`);
+    }
+}
+
+
+//Feature Upgrades
 export function upgradeAutoFeedSpeed(){
-   //To ADD
    let incAFSpeed = upgrades.autoFeed;
     if (gameState.score.greaterThanOrEqualTo(incAFSpeed.cost)){
         //Reduce score
@@ -47,31 +66,58 @@ export function upgradeAutoFeedSpeed(){
         upgrades.autoFeed.level = upgrades.autoFeed.level + 1;
         clearInterval(gameState.dotIntervalID);
         gameState.dotIntervalID = setInterval(createDot, incAFSpeed.speed*1000);
+        $("#upgradeAutoFeedSpeedLvl").text(`Level: ${incAFSpeed.level}`);
     }
 }
 
-export function upgradeDotMulti(){
-    
-    let incDotMulti = upgrades.increaseDotMulti;
-    if (gameState.score.greaterThanOrEqualTo(incDotMulti.cost)){
-        //Reduce score
-        gameState.score = gameState.score.minus(incDotMulti.cost);
-        //Update dot Value
-        gameState.dotMulti = gameState.dotMulti.times(incDotMulti.increase);
-        //Update dot cost
-        incDotMulti.cost = incDotMulti.cost.times(incDotMulti.upgradeScale);
-        //Increase level
-        upgrades.increaseDotMulti.level = upgrades.increaseDotMulti.level+1 ;
-    }
-}
 
-export function upgradeNomDotMulti(){
-    const upgradeData = upgrades.increaseNomDotMulti;
+//Misc Upgrades
+export function increaseNomCoinMulti(){
+   let upgradeData = upgrades.increaseNomCoinMulti;
     if (gameState.nomCoins.greaterThanOrEqualTo(upgradeData.cost)){
+        //Purchase upgrade
         gameState.nomCoins = gameState.nomCoins.minus(upgradeData.cost);
-        gameState.nomscendMultiValue = gameState.nomscendMultiValue.plus(upgradeData.increase);
-        upgrades.increaseNomDotMulti.cost = new Decimal(upgradeData.baseCost.times(Math.pow(upgradeData.upgradeScale, upgradeData.level)).toFixed(2));
-        upgrades.increaseNomDotMulti.level = upgrades.increaseNomDotMulti.level+1;
+        //Update effect
+        gameState.nomCoinMulti = gameState.nomCoinMulti.plus(upgradeData.increase);
+        //Update upgrade cost
+        upgrades.increaseNomCoinMulti.cost = increaseCost(upgradeData.baseCost, upgradeData.upgradeScale, upgradeData.level);
+        //Increase level
+        upgrades.increaseNomCoinMulti.level = upgradeData.level + 1;
+    }
+}
+
+
+//NOM Upgrades
+
+
+export function increaseNomDotMulti(){
+    const upgradeData = upgrades.increaseNomDotMulti;
+    // if (gameState.nomCoins.greaterThanOrEqualTo(upgradeData.cost)){
+    //     //Purchase upgrade
+    //     gameState.nomCoins = gameState.nomCoins.minus(upgradeData.cost);
+    //     //Upgrade effect
+    //         gameState.dotMulti = gameState.dotMulti.plus(gameState.nomscendDotVal);
+    //     gameState.nomscendDotVal = gameState.nomscendDotVal.plus(upgradeData.increase);
+    //     gameState.dotMulti = gameState.dotMulti.plus(gameState.nomscendDotVal);
+    //     //Increase cost
+    //     upgrades.increaseNomDotMulti.cost = new Decimal(upgradeData.baseCost.times(Math.pow(upgradeData.upgradeScale, upgradeData.level)).toFixed(2));
+    //     //Increase upgrade level
+    //     upgrades.increaseNomDotMulti.level = upgradeData.level+1;
+    // }
+}
+export function increaseNomDotVal(){
+    const upgradeData = upgrades.increaseNomDotVal;
+    if (gameState.nomCoins.greaterThanOrEqualTo(upgradeData.cost)){
+        //Purchase upgrade
+        gameState.nomCoins = gameState.nomCoins.minus(upgradeData.cost);
+        //Upgrade effect
+        gameState.dotValue = gameState.dotValue.minus(gameState.nomscendDotVal);
+        gameState.nomscendDotVal = gameState.nomscendDotVal.plus(upgradeData.increase);
+        gameState.dotValue = gameState.dotValue.plus(gameState.nomscendDotVal);
+        //Increase cost
+        upgrades.increaseNomDotVal.cost = increaseCost(upgradeData.baseCost, upgradeData.upgradeScale, upgradeData.level);
+        //Increase upgrade level
+        upgrades.increaseNomDotVal.level = upgradeData.level+1;
     }
 }
 
@@ -79,17 +125,29 @@ export function increaseDotMultiMax(){
     const upgradeData = upgrades.increaseDotMultiMax;
     if (gameState.nomCoins.greaterThanOrEqualTo(upgradeData.cost)){
         gameState.nomCoins = gameState.nomCoins.minus(upgradeData.cost);
-        upgrades.increaseDotMultiMax.cost = new Decimal(upgradeData.baseCost.times(Math.pow(upgradeData.upgradeScale, upgradeData.level)).toFixed(2));
+        upgrades.increaseDotMultiMax.cost = increaseCost(upgradeData.baseCost, upgradeData.upgradeScale, upgradeData.level);
         upgrades.increaseDotMultiMax.level = upgrades.increaseDotMultiMax.level+1;
         upgrades.increaseDotMulti.maxlevel = upgrades.increaseDotMulti.maxlevel+1;
     }
 }
-// export function increaseDotValMax(){
-//     const upgradeData = upgrades.increaseDotValMax;
-//     if (gameState.nomCoins.greaterThanOrEqualTo(upgradeData.cost)){
-//         gameState.nomCoins = gameState.nomCoins.minus(upgradeData.cost);
-//         upgrades.increaseDotMultiMax.cost = new Decimal(upgradeData.baseCost.times(Math.pow(upgradeData.upgradeScale, upgradeData.level)).toFixed(2));
-//         upgrades.increaseDotMultiMax.level = upgrades.increaseDotMultiMax.level+1;
-//         upgrades.increaseDotMulti.maxlevel = upgrades.increaseDotMulti.maxlevel+1;
-//     }
-// }
+export function increaseDotValMax(){
+    const upgradeData = upgrades.increaseDotValMax;
+    if (gameState.nomCoins.greaterThanOrEqualTo(upgradeData.cost)){
+        gameState.nomCoins = gameState.nomCoins.minus(upgradeData.cost);
+        upgrades.increaseDotValMax.cost = increaseCost(upgradeData.baseCost, upgradeData.upgradeScale, upgradeData.level);
+        upgrades.increaseDotValMax.level = upgrades.increaseDotValMax.level+1;
+        upgrades.increaseDotValue.maxlevel = upgrades.increaseDotValue.maxlevel+upgradeData.increase.toNumber();
+    }
+}
+
+export function increaseStartDotSpeedLevel(){
+    const upgradeData = upgrades.increaseStartDotSpeedLevel;
+    if (gameState.nomCoins.greaterThanOrEqualTo(upgradeData.cost)){
+        gameState.nomCoins = gameState.nomCoins.minus(upgradeData.cost);
+        upgrades.increaseStartDotSpeedLevel.cost = increaseCost(upgradeData.baseCost, upgradeData.upgradeScale, upgradeData.level);
+        upgrades.increaseStartDotSpeedLevel.level = upgradeData.level+1;
+        //Upgrade Effect
+        upgrades.increaseStartDotSpeedLevel.level = upgrades.increaseStartDotSpeedLevel.level+upgradeData.increase;
+        upgrade
+    }
+}
