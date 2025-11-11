b870c2fa84bebe1271872f238db38851100a4491
Nicholas Harper <harpergnicholas@gmail.com>
2025-11-11 12:11:48 +0900

    Updated score increase functions to reflect new nomcoinscore boost multiplier.

--- scripts/score.js
+++ scripts/score.js
@@ -1,10 +1,30 @@
 import { gameState } from './data.js';
 
 export function increaseScore(){
-    const value =  gameState.dotValue.times(gameState.dotMulti);
+    //Score increase formula
+    const value =  gameState.dotValue.times(gameState.dotMulti).times(gameState.nomScoreBoostAmount);
+    if (gameState.squares.greaterThan(0)) {
+        value = value.times(gameState.squares);
+    }
+    //Add score and update stats
     gameState.score = gameState.score.plus(value);
     gameState.liftimeScore = gameState.liftimeScore.plus(value);
     gameState.dotsEaten = gameState.dotsEaten.plus(1);
-    gameState.nomscendScore = gameState.nomscendScore.plus(value)
+    gameState.nomscendScore = gameState.nomscendScore.plus(value);
+
+    return value;
+}
+
+export function eatSquare(){
+    gameState.squares = gameState.squares.plus(gameState.squareValue);
+    gameState.squareCount = gameState.squareCount.plus(1);
+    gameState.lifetimeSquares = gameState.lifetimeSquares.plus(1);
+}
+
+export function updateNomScoreBoost(){
+    gameState.nomScoreBoostAmount = new Decimal(gameState.lifetimeNomCoins / 100);
+    if (gameState.nomScoreBoostAmount.lessThan(1)) {
+        gameState.nomScoreBoostAmount = gameState.nomScoreBoostAmount.plus(1);
+    }
 }
 
