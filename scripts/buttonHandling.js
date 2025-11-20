85abcdcc51ac682353fdba3765b13a8270d6d47f
Nicholas Harper <harpergnicholas@gmail.com>
2025-11-20 16:34:40 +0900

    Changed button handling to be more streamlined, both for max buttons and regular upgrades.

--- scripts/buttonHandling.js
+++ scripts/buttonHandling.js
@@ -1,11 +1,12 @@
 import { createDot, createSquare, spawnDot } from './consumables.js';
-import { enableAutofeed, createKids, nomscend, unlockSquares } from './features.js';
-import { upgradeDotValue, upgradeDotSpeed, upgradeAutoFeedSpeed, upgradeDotMulti, upgradeDotSpawnRate, upgradeDotSpawnCount, upgradeDotMaxCount, addRoboNom} from './upgradeButtons.js';
-import { increaseDotMultiMax, increaseDotValMax, increaseNomCoinMulti, increaseNomDotVal, increaseStartDotSpeedLevel, increaseAutoFeedMax, increaseStartDotMultiLevel, increaseDotSpeedBase, keepAutofeed, nomCoinScoreBoost, unlockRoboNoms } from './Upgrades/nomupgrades.js'
-import { upgrades, gameState, shopUpgrades, mouseNom, roboList, mousePos } from './data.js';
-import { calcBuyMax, formatNum, increaseCost, setDotValue, setSpeed, setAutoFeed, setDotMulti  } from './util.js';
+import { enableAutofeed, createKids, nomscend, unlockSquare } from './features.js';
+import { upgradeDotValue, upgradeDotMulti, upgradeDotSpawnRate, upgradeDotSpawnCount, addRoboNom, upgradeMaxDotCount} from './upgradeButtons.js';
+import { unlockNomCoinScoreBoost, unlockRoboNoms, upgradeDotMultiMax, upgradeDotValMax, upgradeNomCoinMulti, upgradeNomDotVal } from './Upgrades/nomupgrades.js'
+import { upgrades, gameState, mouseNom, roboList, mousePos } from './data.js';
+import { calcBuyMax, formatNum, increaseCost  } from './util.js';
 
 import { saveGame } from './gameFiles.js';
+import { upgradeMaxSquareCount, upgradeSquareMulti, upgradeSquareSpawnCount, upgradeSquareSpawnRate, upgradeSquareValue } from './Upgrades/squareUpgrades.js';
 
 //Spawn Dots with Feed button
 $("#feedNomNom").on("click", function () {
@@ -53,10 +54,6 @@ $(window).on("click", function(event) {
 
 //###############
 
-//SHOP UPGRADES
-$("#unlockAutoFeed").on('click',function(){
-    enableAutofeed();
-});
 
 //################
 //Customization buttons (Color)
@@ -101,152 +98,52 @@ function switchRMenu(activeMenu){
     }   
 }
 
+const upgradeActions = {
+    upgradeDotValue,
+    upgradeDotMulti,
+    upgradeDotSpawnRate,
+    upgradeDotSpawnCount,
+    upgradeMaxDotCount,
+    unlockRoboNoms,
+    unlockSquare,
+    upgradeNomDotVal,
+    upgradeDotValMax,
+    upgradeDotMultiMax,
+    addRoboNom,
+    upgradeNomCoinMulti,
+    unlockNomCoinScoreBoost,
+    upgradeSquareValue,
+    upgradeSquareMulti,
+    upgradeSquareSpawnRate,
+    upgradeSquareSpawnCount,
+    upgradeMaxSquareCount
+    
+};
+
 export function handleUpgrade(id) {
-    let nomOfupgrades = 0;
-    switch (id) {
-        //Dot Value
-        case "upgradeDotValue":
-            upgradeDotValue();
-            break;
-        case "upgradeDotValueMaxBttn":
-            dotValueBuyMax();
-            break;
-        //Dot Multi
-        case "upgradeDotMulti":
-            upgradeDotMulti();
-            break;
-        case "upgradeDotMultiMaxBttn":
-            dotMultiBuyMax();
-            break;
-        //Dot Spawn Rate
-        case "upgradeDotSpawnRate":
-            upgradeDotSpawnRate();
-            break;
-        case "upgradeDotSpawnRateMaxBttn":
-            dotSpawnRateBuyMax();
-            break;
-        //Dot Spawn Count
-        case "upgradeDotSpawnCount":
-            upgradeDotSpawnCount();
-            break;
-        case "upgradeDotSpawnCountMaxBttn":
-            dotSpawnCountBuyMax();
-            break;
-        //Dot Max Count
-        case "upgradeMaxDotCount":
-            upgradeDotMaxCount();
-            break;
-        case "upgradeMaxDotCountMaxBttn":
-            dotMaxCountBuyMax();
-            break;
-        
-        case "upgradeDotSpeed":
-            upgradeDotSpeed();
-            break;
-
-        case "unlockAutoFeed":
-            unlockAutoFeed();
-            break;
-
-        case "upgradeAutoFeedSpeed":
-            upgradeAutoFeedSpeed();
-            break;
-
-        //Robo Nom
-        case "unlockRoboNom":
-            unlockRoboNoms();
-            break;
-            
-        case "addRoboNom":
-            addRoboNom();
-            break;
-        case "addRoboNomMaxBttn":
-            addRoboNomBuyMax();
-            break;
-        case "unlockSquare":
-            unlockSquares();
-
-        default:
-            console.warn(`Unhandled upgrade ID: ${id}`);
-            break;
-    }
-}
-
-function dotValueBuyMax(){
-    const numOfupgrades = calcBuyMax(upgrades.increaseDotValue).count;
-    for (let i=1; i<numOfupgrades+1; i++){
-        upgradeDotValue();
-    }
-}
-function dotMultiBuyMax(){
-    //Do nothing if you can't afford an upgrade
-    const numOfupgrades = calcBuyMax(upgrades.increaseDotMulti).count;
-    for (let i=1; i<numOfupgrades+1; i++){
-        upgradeDotMulti();
-    }
-}
-function dotSpawnRateBuyMax(){
-    const numOfupgrades = calcBuyMax(upgrades.increaseDotSpawnRate).count;
-    for (let i=1; i<numOfupgrades+1; i++){
-        upgradeDotSpawnRate();
-    }
-}
-function dotSpawnCountBuyMax(){
-    const numOfupgrades = calcBuyMax(upgrades.increaseDotSpawnCount).count;
-    for (let i=1; i<numOfupgrades+1; i++){
-        upgradeDotSpawnCount();
-    }
-}
-function dotMaxCountBuyMax(){
-    const numOfupgrades = calcBuyMax(upgrades.increaseMaxDotCount).count;
-    for (let i=1; i<numOfupgrades+1; i++){
-        upgradeDotMaxCount();
-    }
-}
-function addRoboNomBuyMax(){
-    const numOfupgrades = calcBuyMax(upgrades.addRoboNom).count;
-    for (let i=1; i<numOfupgrades+1; i++){
-        addRoboNom();
+    const action = upgradeActions[id];
+    if (action) {
+        action();
+    } else {
+        console.warn(`Unknown upgrade ID: ${id}`);
     }
 }
 
 
+export function handleBuyMax(ButtonID){
+    for(let item in upgrades){
+        if (String(ButtonID) == String(upgrades[item].id)+"MaxBttn"){
+            const numOfupgrades = calcBuyMax(upgrades[item]).count;
+            const action = upgradeActions[upgrades[item].id];
+            for (let i=1; i<numOfupgrades+1; i++){
+                action();
+            }
+        }
+    }
 
-//###############
-//Nom Upgrades
-$("#upgradeNomDotMulti").on('click',function(){
-    increaseNomDotMulti();
-});
-$("#upgradeNomDotVal").on('click',function(){
-    increaseNomDotVal();
-});
-$("#upgradeNomCoinMulti").on('click',function(){
-    increaseNomCoinMulti();
-});
-$("#upgradeDotMultiMax").on('click',function(){
-    increaseDotMultiMax();
-});
-$("#upgradeDotValMax").on('click',function(){
-    increaseDotValMax();
-});
-$("#upgradeStartDotSpeedLevel").on('click',function(){
-    increaseStartDotSpeedLevel();
-});
-$("#upgradeAutoFeedMax").on('click',function(){
-    increaseAutoFeedMax();
-});
-$("#upgradeDotSpeedBase").on('click',function(){
-    increaseDotSpeedBase();
-});
-$("#upgradeStartDotMultiLevel").on('click',function(){
-    increaseStartDotMultiLevel();
-});
-$("#keepAutoFeed").on('click',function(){
-    keepAutofeed();
-});
-$("#unlocknomCoinScoreBoost").on('click',function(){
-    nomCoinScoreBoost();
-});
+}
+
+
 
 
 //##############
@@ -303,8 +200,10 @@ $("#nomscendBttn").on('click',function(){
 });
 
 $("#debugBttn").on('click',function(){
-    // gameState.score = gameState.score.plus(1000);
-    unlockSquares();
+    gameState.score = gameState.score.plus(10000);
+    gameState.nomCoins = gameState.nomCoins.plus(1000);
+    handleBuyMax("upgradeDotValueMaxBttn");
+    unlockSquare();
 });
 
 
@@ -335,7 +234,7 @@ export function buttonCheck(){
         value.cost = increaseCost(value);
         if (value.type == "nomCoins"){
             $("#"+value.id).prop("disabled", value.cost.greaterThan(gameState.nomCoins));
-            $("#"+value.id).html(`<img src="${nomCoinImgSrc}"class="icon-nomCoin">${formatNum(value.cost)}`);
+            $("#"+value.id+" .cost-text").html(formatNum(value.cost));
         } else if (value.type == "score" ){
             $("#"+value.id).prop("disabled", value.cost.greaterThan(gameState.score));
             $("#"+value.id).html(`<div class="icon-dot"></div>${formatNum(value.cost)}`);
