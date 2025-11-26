b7aa067782f8b34205bd22e66b76283c4475e87f
Nicholas Harper <harpergnicholas@gmail.com>
2025-11-26 16:23:04 +0900

    Some code cleanup. Still needs to be done properly

--- scripts/upgradeButtons.js
+++ scripts/upgradeButtons.js
@@ -1,51 +1,34 @@
 import { gameState, roboList } from './data.js';
 import { upgrades } from './data.js';
-import { createDot } from './consumables.js';
-import { getCanvasCentre, increaseCost, setDotMaxCount, setDotSpawnCount, setDotSpawnRate, setRoboNoms } from './util.js';
-import { setAutoFeed,setDotMulti,setDotValue,setSpeed } from './util.js';
-import { RoboNom } from './nomnom.js';
+import { increaseCost, setDotMaxCount, setDotSpawnCount, setDotSpawnRate, setRoboNoms } from './util.js';
+import { setAutoFeed,setDotMulti,setDotValue } from './util.js';
 
 
 
 
 export function upgradeDotValue(){
-    let incDotVal = upgrades.increaseDotValue;
-    if (gameState.score.greaterThanOrEqualTo(incDotVal.cost)){
+    let upgrade = upgrades.increaseDotValue;
+    if (gameState.score.greaterThanOrEqualTo(upgrade.cost) && (upgrade.level < upgrade.maxlevel)){
         //Reduce score
-        gameState.score = gameState.score.minus(incDotVal.cost);
-        //Update dot Value
-        gameState.dotValue = gameState.dotValue.plus(incDotVal.increase);
+        gameState.score = gameState.score.minus(upgrade.cost);
         //Update level
-        upgrades.increaseDotValue.level = upgrades.increaseDotValue.level +1;
+        upgrades.increaseDotValue.level++;
         //Update dot cost
-        incDotVal.cost = increaseCost(incDotVal);
+        upgrades.increaseDotValue.cost = increaseCost(upgrade);
         }
+    setDotValue();
 }
 
-
-export function upgradeDotSpeed(){
-    let incDotSpeed = upgrades.increaseDotSpeed;
-    if (gameState.score.greaterThanOrEqualTo(incDotSpeed.cost)){
-        //Reduce score
-        gameState.score = gameState.score.minus(incDotSpeed.cost);
-        //Update dot Value
-        gameState.dotSpeed = gameState.dotSpeed.minus(incDotSpeed.increase);
-        //Update level
-        upgrades.increaseDotSpeed.level = upgrades.increaseDotSpeed.level + 1;
-        //Update dot cost
-        incDotSpeed.cost = increaseCost(incDotSpeed);
-    }
-}
 
 export function upgradeDotMulti(){    
-    let incDotMulti = upgrades.increaseDotMulti;
-    if (gameState.score.greaterThanOrEqualTo(incDotMulti.cost)){
+    let upgrade = upgrades.increaseDotMulti;
+    if (gameState.score.greaterThanOrEqualTo(upgrade.cost) && (upgrade.level < upgrade.maxlevel)){
         //Reduce score
-        gameState.score = gameState.score.minus(incDotMulti.cost);
+        gameState.score = gameState.score.minus(upgrade.cost);
         //Update level
-        upgrades.increaseDotMulti.level = upgrades.increaseDotMulti.level+1;
+        upgrades.increaseDotMulti.level++;
         //Update dot cost
-        incDotMulti.cost = increaseCost(incDotMulti);
+        upgrades.increaseDotMulti.cost = increaseCost(upgrade);
         setDotMulti();
     }
 }
@@ -56,7 +39,7 @@ export function upgradeDotSpawnRate(){
         //Reduce score
         gameState.score = gameState.score.minus(upgrade.cost);
         //Update level
-        upgrades.increaseDotSpawnRate.level = upgrade.level+1;
+        upgrades.increaseDotSpawnRate.level++;
         //Update cost
         upgrades.increaseDotSpawnRate.cost = increaseCost(upgrade);
         //Upgrade effect
@@ -71,7 +54,7 @@ export function upgradeDotSpawnCount(){
         //Reduce score
         gameState.score = gameState.score.minus(upgrade.cost);
         //Update level
-        upgrades.increaseDotSpawnCount.level = upgrade.level+1;
+        upgrades.increaseDotSpawnCount.level++;
         //Update cost
         upgrades.increaseDotSpawnCount.cost = increaseCost(upgrade);
         //Upgrade effect
@@ -86,7 +69,7 @@ export function upgradeMaxDotCount(){
         //Reduce score
         gameState.score = gameState.score.minus(upgrade.cost);
         //Update level
-        upgrades.increaseMaxDotCount.level = upgrade.level+1;
+        upgrades.increaseMaxDotCount.level++;
         //Update cost
         upgrades.increaseMaxDotCount.cost = increaseCost(upgrade);
         //Upgrade effect
@@ -100,7 +83,7 @@ export function addRoboNom(){
         //Reduce score
         gameState.score = gameState.score.minus(upgrade.cost);
         //Update level
-        upgrades.addRoboNom.level = upgrade.level+1;
+        upgrades.addRoboNom.level++;
         //Update cost
         upgrades.addRoboNom.cost = increaseCost(upgrade);
         //Upgrade effect
@@ -109,19 +92,35 @@ export function addRoboNom(){
 }
 
 
-//Feature Upgrades
+//OLD Upgrades
+
+export function upgradeDotSpeed(){
+    let upgrade = upgrades.increaseDotSpeed;
+    if (gameState.score.greaterThanOrEqualTo(upgrade.cost) && (upgrade.level < upgrade.maxlevel)){
+        //Reduce score
+        gameState.score = gameState.score.minus(upgrade.cost);
+        //Update dot Value
+        gameState.dotSpeed = gameState.dotSpeed.minus(upgrade.increase);
+        //Update level
+        upgrades.increaseDotSpeed.level++;
+        //Update dot cost
+        upgrades.increaseDotSpeed.cost = increaseCost(upgrade);
+    }
+}
 export function upgradeAutoFeedSpeed(){
    let incAFSpeed = upgrades.autoFeed;
     if (gameState.score.greaterThanOrEqualTo(incAFSpeed.cost)){
         //Reduce score
         gameState.score = gameState.score.minus(incAFSpeed.cost);
         //Increase level
-        upgrades.autoFeed.level = upgrades.autoFeed.level + 1;
+        upgrades.autoFeed.level++;
         setAutoFeed();
         //Update dot cost
         upgrades.autoFeed.cost = increaseCost(incAFSpeed);
     }
 }
+
+
 
 
 
